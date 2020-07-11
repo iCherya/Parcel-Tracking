65e9b19c9aa338538cf4663bb570f36deec7cc24
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-07-11 18:21:15 +0300

    Nova Poshta support

---
+++ readme.md
@@ -0,0 +1,19 @@
+# 🌤 Weather Forecast
+Weather forecast pet-project with GEO-search using OpenWeatherMap API
+
+![Demo](./img/weather-forecast-demo.gif)
+
+This repo is not contain config.js file with OpenWeatherMap API key. To work with this project you should:
+1. Generate your own API key on [OpenWeatherMap API](https://openweathermap.org/api)
+2. Create config.js file in root folder with:
+```javascript
+const config = {
+    MY_KEY: 'Place here your own API key'
+}
+```
+
+
+**Technology used:**
+* HTML
+* CSS
+* JavaScript
---
+++ script.js
@@ -0,0 +1,62 @@
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return await response.json();
+}
+
+const requestBody = {
+    "apiKey": config.NP_KEY,
+    "modelName": "TrackingDocument",
+    "calledMethod": "getStatusDocuments",
+    "methodProperties": {
+        "Documents": []
+    }
+}
+
+document.querySelector('#clear').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('textarea').value = '';
+});
+
+document.querySelector('#track').addEventListener('click', (e) => {
+    e.preventDefault();
+    const resultBlock = document.querySelector('.result');
+    requestBody.methodProperties.Documents = [];
+    const textArea = document.querySelector('textarea');
+    if (!!textArea.value) {
+        e.target.disabled = true;
+        e.target.textContent = 'Tracking';
+
+        const trackingArr = textArea.value.match(/\w+/gi) || [];
+        textArea.value = '';
+
+        let textAreaOutput = trackingArr.reduce((prev, curr) => {
+            return prev + '\n' + curr;
+        })
+        textArea.value = textAreaOutput;
+
+        trackingArr.forEach(el => {
+            requestBody.methodProperties.Documents.push({
+                "DocumentNumber": el
+            })
+        })
+        postData('https://api.novaposhta.ua/v2.0/json/', requestBody)
+            .then((data) => {
+                resultBlock.innerHTML = showStatus(data);
+                e.target.disabled = false;
+                e.target.textContent = 'Track again';
+            });
+    }
+
+});
+
+function showStatus(resultData) {
+    return resultData.data.reduce((prev, curr) => {
+        return prev + `<b>${curr.Number}</b>: ${curr.Status}<br>`;
+    }, ' ')
+}
